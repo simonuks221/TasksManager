d42e94cc47b9706f88333a88b4efbe877efd7ed8
simonuks221 <simonasriauka@gmail.com>
2019-02-15 10:38:14 +0200

    Fixes

---
+++ TasksManager/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace TasksManager
+{
+    partial class TasksManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tasksPanel = new System.Windows.Forms.Panel();
+            this.TasksManagerLabel = new System.Windows.Forms.Label();
+            this.MainTimeLabel = new System.Windows.Forms.Label();
+            this.NewTaskButton = new System.Windows.Forms.Button();
+            this.CurrentTimeTimer = new System.Windows.Forms.Timer(this.components);
+            this.NewTaskPanel = new System.Windows.Forms.Panel();
+            this.AddNewTaskButton = new System.Windows.Forms.Button();
+            this.NewTaskCancelButton = new System.Windows.Forms.Button();
+            this.NewTaskImportanceComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NewTaskDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.newTaskTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.NewTaskDescriptiontextBox = new System.Windows.Forms.TextBox();
+            this.NewTaskNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SortByComboBox = new System.Windows.Forms.ComboBox();
+            this.useAnimationsCheckBox = new System.Windows.Forms.CheckBox();
+            this.NewTaskPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tasksPanel
+            // 
+            this.tasksPanel.AutoScroll = true;
+            this.tasksPanel.Location = new System.Drawing.Point(56, 113);
+            this.tasksPanel.Name = "tasksPanel";
+            this.tasksPanel.Size = new System.Drawing.Size(687, 322);
+            this.tasksPanel.TabIndex = 1;
+            // 
+            // TasksManagerLabel
+            // 
+            this.TasksManagerLabel.AutoSize = true;
+            this.TasksManagerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.TasksManagerLabel.Location = new System.Drawing.Point(12, 9);
+            this.TasksManagerLabel.Name = "TasksManagerLabel";
+            this.TasksManagerLabel.Size = new System.Drawing.Size(148, 25);
+            this.TasksManagerLabel.TabIndex = 2;
+            this.TasksManagerLabel.Text = "Tasks manager";
+            // 
+            // MainTimeLabel
+            // 
+            this.MainTimeLabel.AutoSize = true;
+            this.MainTimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.MainTimeLabel.Location = new System.Drawing.Point(315, 24);
+            this.MainTimeLabel.Name = "MainTimeLabel";
+            this.MainTimeLabel.Size = new System.Drawing.Size(118, 25);
+            this.MainTimeLabel.TabIndex = 3;
+            this.MainTimeLabel.Text = "Current time";
+            // 
+            // NewTaskButton
+            // 
+            this.NewTaskButton.Location = new System.Drawing.Point(65, 74);
+            this.NewTaskButton.Name = "NewTaskButton";
+            this.NewTaskButton.Size = new System.Drawing.Size(131, 30);
+            this.NewTaskButton.TabIndex = 5;
+            this.NewTaskButton.Text = "New task";
+            this.NewTaskButton.UseVisualStyleBackColor = true;
+            this.NewTaskButton.Click += new System.EventHandler(this.NewTaskButton_Click);
+            // 
+            // CurrentTimeTimer
+            // 
+            this.CurrentTimeTimer.Tick += new System.EventHandler(this.CurrentTimeTimer_Tick);
+            // 
+            // NewTaskPanel
+            // 
+            this.NewTaskPanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.NewTaskPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NewTaskPanel.Controls.Add(this.AddNewTaskButton);
+            this.NewTaskPanel.Controls.Add(this.NewTaskCancelButton);
+            this.NewTaskPanel.Controls.Add(this.NewTaskImportanceComboBox);
+            this.NewTaskPanel.Controls.Add(this.label3);
+            this.NewTaskPanel.Controls.Add(this.NewTaskDatePicker);
+            this.NewTaskPanel.Controls.Add(this.newTaskTimePicker);
+            this.NewTaskPanel.Controls.Add(this.NewTaskDescriptiontextBox);
+            this.NewTaskPanel.Controls.Add(this.NewTaskNameTextBox);
+            this.NewTaskPanel.Controls.Add(this.label2);
+            this.NewTaskPanel.Controls.Add(this.label1);
+            this.NewTaskPanel.Location = new System.Drawing.Point(159, 101);
+            this.NewTaskPanel.Name = "NewTaskPanel";
+            this.NewTaskPanel.Size = new System.Drawing.Size(541, 331);
+            this.NewTaskPanel.TabIndex = 6;
+            // 
+            // AddNewTaskButton
+            // 
+            this.AddNewTaskButton.Location = new System.Drawing.Point(418, 281);
+            this.AddNewTaskButton.Name = "AddNewTaskButton";
+            this.AddNewTaskButton.Size = new System.Drawing.Size(120, 47);
+            this.AddNewTaskButton.TabIndex = 9;
+            this.AddNewTaskButton.Text = "Add task";
+            this.AddNewTaskButton.UseVisualStyleBackColor = true;
+            this.AddNewTaskButton.Click += new System.EventHandler(this.AddNewTaskButton_Click);
+            // 
+            // NewTaskCancelButton
+            // 
+            this.NewTaskCancelButton.Location = new System.Drawing.Point(509, 3);
+            this.NewTaskCancelButton.Name = "NewTaskCancelButton";
+            this.NewTaskCancelButton.Size = new System.Drawing.Size(29, 23);
+            this.NewTaskCancelButton.TabIndex = 8;
+            this.NewTaskCancelButton.Text = "X";
+            this.NewTaskCancelButton.UseVisualStyleBackColor = true;
+            this.NewTaskCancelButton.Click += new System.EventHandler(this.NewTaskCancelButton_Click);
+            // 
+            // NewTaskImportanceComboBox
+            // 
+            this.NewTaskImportanceComboBox.FormattingEnabled = true;
+            this.NewTaskImportanceComboBox.Items.AddRange(new object[] {
+            "Not important",
+            "Normal",
+            "Important",
+            "Extremely important"});
+            this.NewTaskImportanceComboBox.Location = new System.Drawing.Point(400, 50);
+            this.NewTaskImportanceComboBox.Name = "NewTaskImportanceComboBox";
+            this.NewTaskImportanceComboBox.Size = new System.Drawing.Size(121, 21);
+            this.NewTaskImportanceComboBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label3.Location = new System.Drawing.Point(213, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Task deadline";
+            // 
+            // NewTaskDatePicker
+            // 
+            this.NewTaskDatePicker.Location = new System.Drawing.Point(48, 223);
+            this.NewTaskDatePicker.Name = "NewTaskDatePicker";
+            this.NewTaskDatePicker.Size = new System.Drawing.Size(200, 20);
+            this.NewTaskDatePicker.TabIndex = 5;
+            // 
+            // newTaskTimePicker
+            // 
+            this.newTaskTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.newTaskTimePicker.Location = new System.Drawing.Point(264, 223);
+            this.newTaskTimePicker.Name = "newTaskTimePicker";
+            this.newTaskTimePicker.ShowUpDown = true;
+            this.newTaskTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.newTaskTimePicker.TabIndex = 4;
+            // 
+            // NewTaskDescriptiontextBox
+            // 
+            this.NewTaskDescriptiontextBox.Location = new System.Drawing.Point(118, 74);
+            this.NewTaskDescriptiontextBox.Multiline = true;
+            this.NewTaskDescriptiontextBox.Name = "NewTaskDescriptiontextBox";
+            this.NewTaskDescriptiontextBox.Size = new System.Drawing.Size(270, 90);
+            this.NewTaskDescriptiontextBox.TabIndex = 3;
+            // 
+            // NewTaskNameTextBox
+            // 
+            this.NewTaskNameTextBox.Location = new System.Drawing.Point(180, 35);
+            this.NewTaskNameTextBox.Name = "NewTaskNameTextBox";
+            this.NewTaskNameTextBox.Size = new System.Drawing.Size(154, 20);
+            this.NewTaskNameTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label2.Location = new System.Drawing.Point(395, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Task importance";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(223, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "New task\r\n";
+            // 
+            // SortByComboBox
+            // 
+            this.SortByComboBox.FormattingEnabled = true;
+            this.SortByComboBox.Items.AddRange(new object[] {
+            "Default",
+            "Time",
+            "Importance"});
+            this.SortByComboBox.Location = new System.Drawing.Point(622, 74);
+            this.SortByComboBox.Name = "SortByComboBox";
+            this.SortByComboBox.Size = new System.Drawing.Size(121, 21);
+            this.SortByComboBox.TabIndex = 7;
+            this.SortByComboBox.SelectedIndexChanged += new System.EventHandler(this.SortByComboBox_SelectedIndexChanged);
+            // 
+            // useAnimationsCheckBox
+            // 
+            this.useAnimationsCheckBox.AutoSize = true;
+            this.useAnimationsCheckBox.Checked = true;
+            this.useAnimationsCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.useAnimationsCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.useAnimationsCheckBox.Location = new System.Drawing.Point(662, 456);
+            this.useAnimationsCheckBox.Name = "useAnimationsCheckBox";
+            this.useAnimationsCheckBox.Size = new System.Drawing.Size(124, 21);
+            this.useAnimationsCheckBox.TabIndex = 8;
+            this.useAnimationsCheckBox.Text = "Use animations";
+            this.useAnimationsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // TasksManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 489);
+            this.Controls.Add(this.useAnimationsCheckBox);
+            this.Controls.Add(this.SortByComboBox);
+            this.Controls.Add(this.NewTaskPanel);
+            this.Controls.Add(this.NewTaskButton);
+            this.Controls.Add(this.MainTimeLabel);
+            this.Controls.Add(this.TasksManagerLabel);
+            this.Controls.Add(this.tasksPanel);
+            this.Name = "TasksManager";
+            this.Text = "Tasks manager";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TasksManager_FormClosing);
+            this.Load += new System.EventHandler(this.TasksManager_Load);
+            this.NewTaskPanel.ResumeLayout(false);
+            this.NewTaskPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel tasksPanel;
+        private System.Windows.Forms.Label TasksManagerLabel;
+        private System.Windows.Forms.Label MainTimeLabel;
+        private System.Windows.Forms.Button NewTaskButton;
+        private System.Windows.Forms.Timer CurrentTimeTimer;
+        private System.Windows.Forms.Panel NewTaskPanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker newTaskTimePicker;
+        private System.Windows.Forms.TextBox NewTaskDescriptiontextBox;
+        private System.Windows.Forms.TextBox NewTaskNameTextBox;
+        private System.Windows.Forms.ComboBox NewTaskImportanceComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker NewTaskDatePicker;
+        private System.Windows.Forms.Button NewTaskCancelButton;
+        private System.Windows.Forms.Button AddNewTaskButton;
+        private System.Windows.Forms.ComboBox SortByComboBox;
+        private System.Windows.Forms.CheckBox useAnimationsCheckBox;
+    }
+}
+
---
+++ TasksManager/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace TasksManager
+{
+    public partial class TasksManager : Form
+    {
+        List<TaskClass> allTasks = new List<TaskClass>();
+        int currentlySelectedIndex;
+        List<TaskButton> allPlayControlls = new List<TaskButton>();
+
+        public Size taskButtonSize = new Size(650, 30);
+        public Size extendedtaskButtonSize = new Size(650, 130);
+
+        int spaceBetweenTaskButtons = 5;
+
+        public TasksManager()
+        {
+            InitializeComponent();
+        }
+
+        private void TasksManager_Load(object sender, EventArgs e)
+        {
+            CurrentTimeTimer.Start();
+            NewTaskPanel.Visible = false;
+            currentlySelectedIndex = -1;
+            OpenTasksFromFile();
+            SortByTime();
+            SortByComboBox.SelectedIndex = 1;
+        }
+
+        private void NewTaskButton_Click(object sender, EventArgs e) //Add new task
+        {
+            NewTaskDescriptiontextBox.Text = ""; //Reset info fields
+            NewTaskNameTextBox.Text = "";
+            NewTaskImportanceComboBox.SelectedIndex = 0;
+            NewTaskDatePicker.Value = DateTime.Now;
+            newTaskTimePicker.Value = DateTime.Now;
+            NewTaskPanel.Visible = true;
+
+            NewTaskNameTextBox.Select();
+        }
+
+        private void SortByComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SortTaskList();
+        }
+
+        private void TasksManager_FormClosing(object sender, FormClosingEventArgs e) //Save on form close
+        {
+            SaveTasksToFile();
+        }
+
+        private void AddNewTaskButton_Click(object sender, EventArgs e) //Add task completed
+        {
+            if (NewTaskImportanceComboBox.SelectedIndex != -1 && NewTaskNameTextBox.Text != "")
+            {
+                TaskImportance taskImportance = (TaskImportance)NewTaskImportanceComboBox.SelectedIndex;
+                DateTime taskDateTime = TimeHelper.ChangeTime(NewTaskDatePicker.Value, newTaskTimePicker.Value.Hour, newTaskTimePicker.Value.Minute, newTaskTimePicker.Value.Second, newTaskTimePicker.Value.Millisecond);
+
+                TaskClass newTask = new TaskClass(NewTaskNameTextBox.Text, NewTaskDescriptiontextBox.Text, taskImportance, taskDateTime);
+                allTasks.Add(newTask);
+
+                SortTaskList();
+
+                NewTaskPanel.Visible = false;
+                SaveTasksToFile();
+            }
+        }
+
+        void SortTaskList()
+        {
+            switch (SortByComboBox.SelectedIndex)
+            {
+                case 1: //Time
+                    SortByTime();
+                    break;
+                case 2: //Importance
+                    SortByImportance();
+                    break;
+            }
+            DisplayAllTasks();
+        }
+
+        void SortByImportance()
+        {
+            bool repeat = true;
+            while (repeat)
+            {
+                repeat = false;
+                for (int i = 0; i < allTasks.Count - 1; i++)
+                {
+                    if(allTasks[i + 1].taskImportance > allTasks[i].taskImportance) //Swap by importance
+                    {
+                        TaskClass t = allTasks[i + 1];
+                        allTasks[i + 1] = allTasks[i];
+                        allTasks[i] = t;
+                        repeat = true;
+                    }
+                }
+            }
+        }
+
+        void SortByTime()
+        {
+            bool repeat = true;
+            while (repeat)
+            {
+                repeat = false;
+                for(int i = 0; i < allTasks.Count - 1; i++)
+                {
+                    if(allTasks[i].taskDateAndTime > allTasks[i + 1].taskDateAndTime)
+                    {
+                        TaskClass taskClass = allTasks[i + 1];
+                        allTasks[i + 1] = allTasks[i];
+                        allTasks[i] = taskClass;
+                        repeat = true;
+                    }
+                }
+            }
+        }
+
+        void DisplayAllTasks()
+        {
+            currentlySelectedIndex = -1;
+            foreach (Control c in allPlayControlls) //Remove all tasks first
+            {
+                c.Dispose();
+            }
+            allPlayControlls.Clear();
+
+            for (int i = 0; i < allTasks.Count; i++)
+            {
+                TaskButton taskButton = new TaskButton(allTasks[i], i, this);
+                tasksPanel.Controls.Add(taskButton);
+                taskButton.Location = new Point(0, i * 30 + i * 5);
+                allPlayControlls.Add(taskButton);
+            }
+        }
+
+        private void CurrentTimeTimer_Tick(object sender, EventArgs e) //Display and update current time
+        {
+            MainTimeLabel.Text = DateTime.Now.ToString();
+
+            for(int i = 0; i < allTasks.Count; i++)
+            {
+                bool positive;
+                allTasks[i].taskTimeLeft = TimeHelper.TimeLeft(allTasks[i].taskDateAndTime, out positive);
+                allTasks[i].positiveTime = positive;
+                if (positive)
+                {
+                    Console.Out.WriteLine(allTasks[i].taskTimeLeft.Minutes);
+                    if(allTasks[i].taskTimeLeft.Minutes == 1)
+                    {
+                        NotifyIcon notifyIcon = new NotifyIcon();
+                        notifyIcon.Visible = true;
+                        notifyIcon.BalloonTipText = allTasks[i].taskName + " ends in: " + TimeHelper.TimeToString(allTasks[i].taskTimeLeft);
+                        notifyIcon.Icon = SystemIcons.Information;
+
+                        notifyIcon.ShowBalloonTip(30000);
+                        notifyIcon.Dispose();
+                    }
+                    
+                }
+                allPlayControlls[i].UpdateLeftAmountOfTime();
+            }
+        }
+
+        public void TaskButtonPressed(int _index) //Task selected
+        {
+            if(_index == currentlySelectedIndex) //Same task selected
+            {
+                taskButtonAnimation(currentlySelectedIndex, false);
+                currentlySelectedIndex = -1;
+            }
+            else //Different task selected
+            {
+                if(currentlySelectedIndex != -1) //had one already selected
+                {
+                    taskButtonAnimation(currentlySelectedIndex, false);
+                }
+                currentlySelectedIndex = _index;
+
+                taskButtonAnimation(currentlySelectedIndex, true);
+            }
+        }
+
+        private void NewTaskCancelButton_Click(object sender, EventArgs e) //Hide new new task panel
+        {
+            NewTaskPanel.Visible = false;
+        }
+
+        public void RemoveTask(int index)
+        {
+            allTasks.RemoveAt(index);
+            SaveTasksToFile();
+            DisplayAllTasks();
+        }
+
+        void SaveTasksToFile()
+        {
+            Stream stream = File.Open("tasksData.dat", FileMode.Create); //if doesnt exist, create
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, allTasks);
+            stream.Close();
+        }
+
+        void OpenTasksFromFile()
+        {
+            if (File.Exists("tasksData.dat")) //Check if file exists
+            {
+                using (Stream stream = File.Open("tasksData.dat", FileMode.Open))
+                {
+                    var bformatter = new BinaryFormatter();
+                    try
+                    {
+                        allTasks = (List<TaskClass>)bformatter.Deserialize(stream);
+                    }
+                    catch (SerializationException) //File is corrupted, delete it and create a new one
+                    {
+                        stream.Close();
+                        File.Delete("tasksData.dat");
+                        SaveTasksToFile();
+                    }
+                    stream.Close();
+                }
+            }
+            else //File doesnt exist, create new
+            {
+                SaveTasksToFile();
+            }
+        }
+
+        void taskButtonAnimation(int index, bool extend)
+        {
+            if (allPlayControlls.Count > index)
+            {
+                if (useAnimationsCheckBox.Checked)
+                {
+                    for (int i = 0; i < 20; i++)
+                    {
+                        int selectedSizeY = (extendedtaskButtonSize.Height - taskButtonSize.Height) / 20;
+                        if (extend)
+                        {
+                            allPlayControlls[index].Size = new Size(extendedtaskButtonSize.Width, allPlayControlls[index].Size.Height + selectedSizeY);
+                            allPlayControlls[index].Refresh();
+
+                            for (int y = index + 1; y < allPlayControlls.Count; y++)
+                            {
+                                allPlayControlls[y].Location = new Point(0, allPlayControlls[y].Location.Y + selectedSizeY);
+                            }
+                        }
+                        else
+                        {
+                            allPlayControlls[index].Size = new Size(taskButtonSize.Width, allPlayControlls[index].Size.Height - selectedSizeY);
+                            allPlayControlls[index].Refresh();
+
+                            for (int y = index + 1; y < allPlayControlls.Count; y++)
+                            {
+                                allPlayControlls[y].Location = new Point(0, allPlayControlls[y].Location.Y - selectedSizeY);
+                            }
+                        }
+                    }
+                }
+                else //Dont use animations
+                {
+                    if (extend)
+                    {
+                        allPlayControlls[index].Size = extendedtaskButtonSize;
+                        for (int i = index + 1; i < allPlayControlls.Count; i++) //Set all lower tasks location
+                        {
+                            allPlayControlls[i].Location = new Point(0, i * extendedtaskButtonSize.Height + i * spaceBetweenTaskButtons);
+                        }
+                    }
+                    else
+                    {
+                        allPlayControlls[index].Size = taskButtonSize; //reset all tasks location
+                        for (int i = index + 1; i < allPlayControlls.Count; i++)
+                        {
+                            allPlayControlls[i].Location = new Point(0, i * taskButtonSize.Height + i * spaceBetweenTaskButtons);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static class TimeHelper //Helps to set time
+    {
+        public static DateTime ChangeTime(this DateTime dateTime, int hours, int minutes, int seconds, int milliseconds)
+        {
+            return new DateTime(
+                dateTime.Year,
+                dateTime.Month,
+                dateTime.Day,
+                hours,
+                minutes,
+                seconds,
+                milliseconds,
+                dateTime.Kind);
+        }
+
+        public static string TimeToString(TimeSpan timeSpan) //Convert time to readable string
+        {
+            string timeString = "";
+            if (timeSpan > TimeSpan.Zero) { timeString += "Time left: "; } else { timeString += "Time behind: "; }
+            timeSpan = timeSpan.Duration();
+            if (timeSpan.Days != 0) timeString += timeSpan.Days + "d ";
+            if (timeSpan.Hours != 0) timeString += timeSpan.Hours + "h ";
+            if (timeSpan.Minutes != 0) timeString += timeSpan.Minutes + "min ";
+            if (timeSpan.Seconds != 0) timeString += timeSpan.Seconds + "s ";
+
+            return timeString;
+        }
+
+        public static TimeSpan TimeLeft(DateTime _dateTime, out bool positive)
+        {
+            TimeSpan timeSpan = _dateTime - DateTime.Now;
+
+            positive = timeSpan > TimeSpan.Zero;
+            return timeSpan;
+        }
+    }
+}
